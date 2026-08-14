45897ff3056a7477e66349e40ebe4ea3c22f41cf
Harshita Pathania <harshitapathania@Harshitas-MacBook-Air-2.local>
2026-08-14 12:14:45 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://campuscrate-1bl2.onrender.com/api",
 });
 
 export default API;
